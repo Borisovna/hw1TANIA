eb607a23cde8effeb15e0902b14eb719e136f1c0
Tania <0121mymail@gmail.com>
2017-10-19 00:04:14 +0300

    switch исправила в 5

--- task5.php
+++ task5.php
@@ -1,15 +1,24 @@
 <?php
 
-$day = random_int(1, 8);
-switch ($day) {
-    case ($day<=5):
+$day = random_int(1, 10);
+
+if ($day<=5){
+    $a=1;
+} elseif ($day>=5 and $day<=7){
+    $a=2;
+}else{
+    $a=3;
+}
+
+switch ($a) {
+    case 1:
         echo "Это   рабочий   день";
         break;
-    case ($day>=5 and $day<=7):
+    case 2:
         echo "Это   выходной   день";
         break;
-    case ($day<7):
-        echo "Это   выходной   день";
+    case 3:
+        echo "Неизвестный   день";
         break;
 }
 
