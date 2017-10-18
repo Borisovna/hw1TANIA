f02015c3c18d1a1f0f79a4b34f035143cd7e30aa
Tania <0121mymail@gmail.com>
2017-10-18 23:42:40 +0300

    исправила замечания наставника 10.10.17

--- task5.php
+++ task5.php
@@ -1,37 +1,15 @@
 <?php
 $day = random_int(1, 8);
 switch ($day) {
-    case 1:
+    case ($day<=5):
         echo "Это   рабочий   день";
         break;
-    case 2:
-        echo "Это   рабочий   день";
-        break;
-    case 3:
-        echo "Это   рабочий   день";
-        break;
-    case 4:
-        echo "Это   рабочий   день";
-        break;
-    case 5:
-        echo "Это   рабочий   день";
-        break;
-    case 6:
+    case ($day>=5 and $day<=7):
         echo "Это   выходной   день";
         break;
-    case 7:
+    case ($day<7):
         echo "Это   выходной   день";
-        break;
-    default:
-        echo "такого дня недели не бывает";
         break;
 }
 
 
-
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 08.10.2017
- * Time: 22:33
- */
