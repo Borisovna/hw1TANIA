3358f717c65d5d7489bc1d7329e0ea30da88ee46
Tania <0121mymail@gmail.com>
2017-10-19 19:41:02 +0300

    switch исправила в 5-от , убрала if

--- task5.php
+++ task5.php
@@ -2,22 +2,32 @@
 
 $day = random_int(1, 10);
 
-if ($day<=5){
-    $a=1;
-} elseif ($day>=5 and $day<=7){
-    $a=2;
-}else{
-    $a=3;
-}
-
 switch ($a) {
     case 1:
         echo "Это   рабочий   день";
         break;
+    case 1:
+        echo "Это   рабочий   день";
+        break;
     case 2:
-        echo "Это   выходной   день";
+        echo "Это   рабочий   день";
         break;
     case 3:
+        echo "Это   рабочий   день";
+        break;
+    case 4:
+        echo "Это   рабочий   день";
+        break;
+    case 5:
+        echo "Это   рабочий   день";
+        break;
+    case 6:
+        echo "Это   выходной   день";
+        break;
+    case 7:
+        echo "Это   выходной   день";
+        break;
+    default:
         echo "Неизвестный   день";
         break;
 }
